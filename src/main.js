6d6cd6ec9943e619b294a6b493f87582ce857781
DaLVeRS2001 <59927513+DaLVeRS2001@users.noreply.github.com>
2021-06-13 02:27:02 +0300

    add dev

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 
+import Vue from "@vue/cli-plugin-eslint/eslintOptions";
+Vue.config.devtools = true
 
 const app = createApp(App)
   app.mount("#app")
